d448d6e31529d9a7a4086c6a47f4f1e7fcd5c7b2
Rudolph Lombaard <prlombaard@gmail.com>
2020-12-04 19:57:41 +0200

    Initial commit, RFM95 PINS modified

---
+++ src/secrets.h
@@ -0,0 +1,5 @@
+// OTAA credentials
+//the following details can be procured from https://console.thethingsnetwork.org/applications/ devices overview webpage                    
+const char *devEui = "0000000000000000";
+const char *appEui = "0000000000000000";
+const char *appKey = "00000000000000000000000000000000";
